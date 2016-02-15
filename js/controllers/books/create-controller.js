39841344a1ad364ca6ad256cdcbc62b2d5cc8233
Nouran Mahmoud <nouran.mhmoud@gmail.com>
2016-02-16 01:26:22 +0200

    change addNew function name to save

--- js/controllers/books/create-controller.js
+++ js/controllers/books/create-controller.js
@@ -3,7 +3,7 @@
   .controller('createController', createController)
 
   function createController($scope, $location){
-    $scope.addNewBook = function(model){
+    $scope.saveBook = function(model){
       library_books.push(model);
       model.id = library_books.length
       $location.path('/book/'+model.id);
